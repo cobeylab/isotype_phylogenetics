c5abd3a12148b0f8a7fd1069de83bcb3dd8f7dd8
Marcos Costa Vieira <mvieira@midway2-login2.rcc.local>
2017-08-03 11:14:14 -0500

    Fixed bug in sequence processing script

--- src/process_raw_sequences.py
+++ src/process_raw_sequences.py
@@ -7,6 +7,7 @@ import re
 import sys
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna
+from Bio import SeqIO
 
 
 raw_file_names = ['../data/01107PB.fasta', '../data/01207PB.fasta', '../data/SFAPB.fasta']
@@ -20,35 +21,21 @@ def main(argv):
 
     for file_name in raw_file_names:
 
-        print 'Processing ' + file_name
+        print 'Processing ' + file_name.replace('../data/','')
 
         processed_file_name = file_name.replace('.fasta','_processed.fasta')
 
         first_line = True
 
-        with open(file_name, 'r') as raw_file, open(processed_file_name,'w') as processed_file:
-            for line in raw_file:
-                #If line contains a sequence identifier:
-                if line.find('>') > -1:
-                    seq_id = re.search(r'>[A-Z|0-9]*', line)
-                    seq_id = seq_id.group()
+        with open(file_name, 'rU') as raw_file, open(processed_file_name,'w') as processed_file:
 
-                    if first_line == True:
-                        processed_file.write(seq_id + '\n')
-                        first_line = False
+            fasta_object = list(SeqIO.parse(raw_file, "fasta"))
 
-                    else:
-                        processed_file.write('\n' + seq_id + '\n')
+            for record in fasta_object:
+                record.seq = record.seq.reverse_complement()
+                record.description = ''
 
-                #If not, then it contains a sequence:
-                else:
-                    sequence = line.replace('\n','')
-                    sequence = Seq(sequence, generic_dna)
-                    sequence = sequence.reverse_complement()
-
-                    #sequence.translate(table=1)
-
-                    processed_file.write(str(sequence))
+            SeqIO.write(fasta_object, processed_file, 'fasta')
 
 
 if(__name__ == "__main__"):
