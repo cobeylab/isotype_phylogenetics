519cfb67b27751ae91b6306689373b3d9aec3d07
Marcos Costa Vieira <mvieira@midway2-login2.rcc.local>
2017-08-08 17:40:57 -0500

    Sbatch and shell files for submitting isotype assignment jobs for the 01207PB and SFAPB datasets

--- sbatch_files/submit_Cregion_blast_SFAPB.sh
+++ sbatch_files/submit_Cregion_blast_SFAPB.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+#!/bin/bash
+
+
 # Get all clone identifiers
-ls ../results/clones/01107PB_clone_* | grep -v 'macse' | grep -o 'clone_[0-9]*'|tr -d [a-z_] > clone_ids_01107PB_blast.tmp
+ls ../results/clones/SFAPB_clone_* | grep -v 'macse' | grep -o 'clone_[0-9]*'|tr -d [a-z_] > clone_ids_SFAPB_blast.tmp
 
 # Split clone ids in files with 500 ids (so that sbatch commands do not become too long)
-split -l 500 clone_ids_01107PB_blast.tmp 'clone_ids_01107PB_blast' -a 1
-rm clone_ids_01107PB_blast.tmp
+split -l 500 clone_ids_SFAPB_blast.tmp 'clone_ids_SFAPB_blast' -a 1
+rm clone_ids_SFAPB_blast.tmp
 
 # Loop over aggregated clone id files, submitting array jobs
-for f in clone_ids_01107PB_blast*
+for f in clone_ids_SFAPB_blast*
 do
     #Get clone identifiers, concatenate into single variable with ',' for sbatch array:
     CLONE_IDS=$(cat $f | tr '\n' ',')
@@ -17,8 +20,8 @@ do
     CLONE_IDS=${CLONE_IDS::-1}
 
     # Run sbatch command passing clone ids to --array option
-    sbatch --array=$CLONE_IDS Cregion_blast_01107PB.sbatch
+    sbatch --array=$CLONE_IDS Cregion_blast_SFAPB.sbatch
 done
 
 # Remove temporary files
-rm clone_ids_01107PB_blast*
+rm clone_ids_SFAPB_blast*
