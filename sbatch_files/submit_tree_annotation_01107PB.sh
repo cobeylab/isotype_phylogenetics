c0d3f4325d161b0565e00bdcb3be5b7a6c040ed6
Marcos Costa Vieira <mvieira@midway2-login2.rcc.local>
2017-08-14 11:51:08 -0500

    Scripts for submitting tree annotation jobs

---
+++ sbatch_files/submit_tree_annotation_01107PB.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get clone identifiers for all clones with raxml best trees
+ls ../results/trees/RAxML_bestTree.01107PB_clone_* | grep -o 'clone_[0-9]*'|tr -d [a-z_] > clone_ids_01107PB_annotation.tmp
+
+# Split clone ids in files with 500 ids (so that sbatch commands do not become too long)
+split -l 500 clone_ids_01107PB_annotation.tmp 'clone_ids_01107PB_annotation' -a 1
+rm clone_ids_01107PB_annotation.tmp
+
+# Loop over aggregated clone id files, submitting array jobs
+for f in clone_ids_01107PB_annotation*
+do
+    #Get clone identifiers, concatenate into single variable with ',' for sbatch array:
+    CLONE_IDS=$(cat $f | tr '\n' ',')
+    
+    # Remove trailing comma
+    CLONE_IDS=${CLONE_IDS::-1}
+
+    # Run sbatch command passing clone ids to --array option
+    sbatch --array=$CLONE_IDS annotate_trees_01107PB.sbatch
+done
+
+# Remove temporary files
+rm clone_ids_01107PB_annotation*
