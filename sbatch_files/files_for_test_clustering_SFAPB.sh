4ad65a8a05233468a0e19e06b7d7a133f86c5d4e
Kang Chon Kim <kckim@midway2-login1.rcc.local>
2017-08-16 07:56:05 -0500

    adding files for test clustering

---
+++ sbatch_files/files_for_test_clustering_SFAPB.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#This file contains summaries of clustering statistics of all clones
+SMR_FILE=../results/test_clustering/summary_stats_clustering_SFAPB.csv
+
+count=0
+
+for f in $(ls ../results/test_clustering/stats_clustering_SFAPB_clone_*txt)
+do
+	CLONE_ID=$(echo $f | grep -o 'clone_[0-9]*')
+
+	#write the summary line (which is the last line of stats_clustering*.txt file) to the summary file
+	#for the first stats_clustering*.txt file, write last 2 lines to the summary file to write a head
+	if [ $count = 0 ]; then
+		cat $f | tail -n 2 > $SMR_FILE
+	else
+		cat $f | tail -n 1 >> $SMR_FILE
+	fi
+
+	#For each clone, write a file (stats_clustering_*_rdm.csv) that contains 1000 randomized clustering statistics
+	RDM_FILE=../results/test_clustering/stats_clustering_SFAPB_${CLONE_ID}_rdm.csv
+	#except for last 2 lines, write all lines to stats_clustering_*_rdm.csv file
+	cat $f | head -n -2 > $RDM_FILE
+
+	count=$((count+1))
+done
